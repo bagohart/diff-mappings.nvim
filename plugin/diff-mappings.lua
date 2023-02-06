9510b9b3dadb52dc42da5d6d31d35ca348ad5672
Bagohart <bagohart@gmx.de>
2023-02-06 09:20:01 +0100

    added visual mode mappings and changed plug naming to include 'mappings'

--- plugin/diff-mappings.lua
+++ plugin/diff-mappings.lua
@@ -1,36 +1,60 @@
-vim.keymap.set('n', '<Plug>(git-diff-plus)', function()
+vim.keymap.set('n', '<Plug>(diff-mappings-n-plus)', function()
     vim.cmd([[keeppatterns substitute/\v^./+/e]])
     vim.cmd([[normal! ``]])
-    vim.fn['repeat#set'](vim.api.nvim_replace_termcodes('<Plug>(git-diff-plus)', true, false, true))
+    vim.fn['repeat#set'](vim.api.nvim_replace_termcodes('<Plug>(diff-mappings-n-plus)', true, false, true))
 end)
 
-vim.keymap.set('n', '<Plug>(git-diff-minus)', function()
+vim.keymap.set('n', '<Plug>(diff-mappings-n-minus)', function()
     vim.cmd([[keeppatterns substitute/\v^./-/e]])
     vim.cmd([[normal! ``]])
-    vim.fn['repeat#set'](vim.api.nvim_replace_termcodes('<Plug>(git-diff-minus)', true, false, true))
+    vim.fn['repeat#set'](vim.api.nvim_replace_termcodes('<Plug>(diff-mappings-n-minus)', true, false, true))
 end)
 
-vim.keymap.set('n', '<Plug>(git-diff-context)', function()
+vim.keymap.set('n', '<Plug>(diff-mappings-n-context)', function()
     vim.cmd([[keeppatterns substitute/\v^./ /e]])
     vim.cmd([[normal! ``]])
-    vim.fn['repeat#set'](vim.api.nvim_replace_termcodes('<Plug>(git-diff-context)', true, false, true))
+    vim.fn['repeat#set'](vim.api.nvim_replace_termcodes('<Plug>(diff-mappings-n-context)', true, false, true))
 end)
 
-vim.keymap.set('n', '<Plug>(diff-mappings-next-change)', function()
+vim.keymap.set('x', '<Plug>(diff-mappings-x-plus)', function()
+    local view = vim.fn.winsaveview()
+    local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
+    vim.api.nvim_feedkeys(esc, 'x', false)
+    vim.cmd([['<,'>normal! 0r+]])
+    vim.fn.winrestview(view)
+end, { buffer = true })
+
+vim.keymap.set('x', '<Plug>(diff-mappings-x-minus)',function()
+    local view = vim.fn.winsaveview()
+    local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
+    vim.api.nvim_feedkeys(esc, 'x', false)
+    vim.cmd([['<,'>normal! 0r-]])
+    vim.fn.winrestview(view)
+end , { buffer = true })
+
+vim.keymap.set('x', '<Plug>(diff-mappings-x-context)',function()
+    local view = vim.fn.winsaveview()
+    local esc = vim.api.nvim_replace_termcodes('<esc>', true, false, true)
+    vim.api.nvim_feedkeys(esc, 'x', false)
+    vim.cmd([['<,'>normal! 0r ]])
+    vim.fn.winrestview(view)
+end, { buffer = true })
+
+vim.keymap.set('n', '<Plug>(diff-mappings-mappings-next-change)', function()
     local save_cursor = vim.fn.getcurpos()
     vim.fn.search([==[\v^[^+-]]==], 'cW')
     local line = vim.fn.search([==[\v^(\+|\-)]==], 'cW')
     if line == 0 then vim.fn.setpos('.', save_cursor) end
 end, { buffer = true, desc = "Go to next line starting with '+' or '-'" })
 
-vim.keymap.set('n', '<Plug>(diff-mappings-previous-change)', function()
+vim.keymap.set('n', '<Plug>(diff-mappings-mappings-previous-change)', function()
     local save_cursor = vim.fn.getcurpos()
     vim.fn.search([==[\v^[^+-]]==], 'bcW')
     local line = vim.fn.search([==[\v^(\+|\-)]==], 'bcW')
     if line == 0 then vim.fn.setpos('.', save_cursor) end
 end, { buffer = true, desc = "Go to previous line starting with '+' or '-'" })
 
-vim.keymap.set('x', '<Plug>(diff-mappings-x-i-plus)', function()
+vim.keymap.set('x', '<Plug>(diff-mappings-mappings-x-i-plus)', function()
     vim.cmd([[execute "normal! \<Esc>"]])
     local match_last_line = vim.fn.search([==[\v^\+\ze.*\n^[^+]]==], 'cW')
     if match_last_line == 0 then
@@ -42,7 +66,7 @@ vim.keymap.set('x', '<Plug>(diff-mappings-x-i-plus)', function()
     end
 end, { buffer = true, desc = "Select current or next block of contiguous '+' lines" })
 
-vim.keymap.set('x', '<Plug>(diff-mappings-x-i-minus)', function()
+vim.keymap.set('x', '<Plug>(diff-mappings-mappings-x-i-minus)', function()
     vim.cmd([[execute "normal! \<Esc>"]])
     local match_last_line = vim.fn.search([==[\v^\-\ze.*\n^[^-]]==], 'cW')
     if match_last_line == 0 then
